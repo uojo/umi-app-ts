b25d027d3e45d27a5d41e4fbf7104c9cdf6dfb46
uojo <friend1020@163.com>
2019-09-05 14:02:04 +0800

    feat: husky 使用的 lint 指令变更

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,9 +26,6 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  ecmaFeatures: {
-    jsx: true, // Allows for the parsing of JSX
-  },
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
--- src/pages/page1/page1-1/_a.tsx
+++ src/pages/page1/page1-1/_a.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from './index.css'
 
 export default function() {
   return <div>Hello11</div>
